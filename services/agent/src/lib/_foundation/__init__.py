18d1dbd21b9717eae660de3d4bc0a173fffa22a8
Ayyappa Swamy Thatavarthy <athatavarthy@nvidia.com>
2026-07-09 14:32:56 +0530

    chore(search): keep agent integration out of vss-cli PR
    Signed-off-by: Ayyappa Swamy Thatavarthy <athatavarthy@nvidia.com>

--- services/agent/src/lib/_foundation/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-"""Private, dependency-neutral building blocks for VSS libraries."""
-
-from .errors import BackendUnreachableError
-from .errors import LibraryError
-
-__all__ = ["BackendUnreachableError", "LibraryError"]
