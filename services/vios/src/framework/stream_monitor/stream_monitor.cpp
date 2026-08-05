e8db69a1c164144473dc52e01ef6063b02be3553
gous <gmulla@nvidia.com>
2026-08-05 17:56:20 +0530

    Fix cpp:S1048: do not throw from destructor in stream_monitor
      stream_monitor.cpp:477  SonarQube-Issue: 40c95ac9-846d-4c28-a9c9-0ffb144e0802
    
    Signed-off-by: gous <gmulla@nvidia.com>

--- services/vios/src/framework/stream_monitor/stream_monitor.cpp
+++ services/vios/src/framework/stream_monitor/stream_monitor.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2021-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -473,8 +473,18 @@ public:
     }
     virtual ~QosMeasurementRecord()
     {
-        LOG(info) << "~QosMeasurementRecord - " << m_fName << endl;
-        stopQoS();
+        try
+        {
+            LOG(info) << "~QosMeasurementRecord - " << m_fName << endl;
+            stopQoS();
+        }
+        catch (const std::exception& e)
+        {
+            LOG(error) << "~QosMeasurementRecord - exception: " << e.what() << endl;
+        }
+        catch (...)
+        {
+        }
     }
 
     void startQoS(const string& url, RTPSource* rtpSrcProxy)
