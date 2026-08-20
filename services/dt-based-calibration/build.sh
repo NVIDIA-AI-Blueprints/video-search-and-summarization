62948b36c3c80e00b346a74f54b684bb15aeac4f
tnoumessi <tnoumessi@nvidia.com>
2026-08-20 12:38:30 -0400

    Update license
    Signed-off-by: tnoumessi <tnoumessi@nvidia.com>

--- services/dt-based-calibration/build.sh
+++ services/dt-based-calibration/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 docker build -t autocalibration-server .
 
