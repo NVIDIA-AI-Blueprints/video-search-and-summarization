a50266c11396190e3717391709864865f3845b6b
Venkatasundarakumar.T.S <vts@nvidia.com>
2026-09-03 09:27:04 +0000

    chore(rtvi-vlm): use Apache SPDX headers for new files
    Signed-off-by: Venkatasundarakumar.T.S <vts@nvidia.com>

--- services/rtvi/rt-vlm/docker/rtvi_vlm/patches/apply_vllm_parakeet_transformers5_patch.py
+++ services/rtvi/rt-vlm/docker/rtvi_vlm/patches/apply_vllm_parakeet_transformers5_patch.py
@@ -1,6 +1,6 @@
 ######################################################################################################
 # SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+# SPDX-License-Identifier: Apache-2.0
 ######################################################################################################
 
 """Make vLLM's Parakeet config compatible with Transformers 5 dataclasses."""
--- services/rtvi/rt-vlm/docker/rtvi_vlm/patches/apply_vllm_qwen3_omni_audio_patch.py
+++ services/rtvi/rt-vlm/docker/rtvi_vlm/patches/apply_vllm_qwen3_omni_audio_patch.py
@@ -1,6 +1,6 @@
 ######################################################################################################
 # SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+# SPDX-License-Identifier: Apache-2.0
 ######################################################################################################
 
 """Patch vLLM's Qwen3-Omni video-only processor compatibility bug.
