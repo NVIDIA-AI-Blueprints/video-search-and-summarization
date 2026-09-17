f49e07abbf2b5cd4c3b94832522e3ba5ae20773c
mkothari-commits <mkothari@nvidia.com>
2026-09-17 17:17:23 +0530

    fix(osrb): relicense test_warehouse_timestamp_etc_manual.py to Apache-2.0

--- services/rtvi/rt-vlm/tests/model/openai_compat/internal/test_warehouse_timestamp_etc_manual.py
+++ services/rtvi/rt-vlm/tests/model/openai_compat/internal/test_warehouse_timestamp_etc_manual.py
@@ -1,14 +1,19 @@
 ################################################################################
 #  SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
 #  All rights reserved.
-#  SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+#  SPDX-License-Identifier: Apache-2.0
 #
-#  NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
-#  property and proprietary rights in and to this material, related
-#  documentation and any modifications thereto. Any use, reproduction,
-#  disclosure or distribution of this material and related documentation
-#  without an express license agreement from NVIDIA CORPORATION or
-#  its affiliates is strictly prohibited.
+#  All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
+#  All rights reserved. you may not use this file except in compliance with the License.
+#  All rights reserved. You may obtain a copy of the License at
+#  All rights reserved.
+#  All rights reserved. http://www.apache.org/licenses/LICENSE-2.0
+#  All rights reserved.
+#  All rights reserved. Unless required by applicable law or agreed to in writing, software
+#  All rights reserved. distributed under the License is distributed on an "AS IS" BASIS,
+#  All rights reserved. WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  All rights reserved. See the License for the specific language governing permissions and
+#  All rights reserved. limitations under the License.
 ################################################################################
 
 # PYTHONPATH=src pytest tests/
